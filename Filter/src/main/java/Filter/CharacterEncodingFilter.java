5f7881a4bd37e0d013d9634184b437e99278f908
Falcone-H <71223787+Falcone-H@users.noreply.github.com>
2021-04-13 15:41:18 +0800

    过滤器

---
+++ Filter/src/main/java/Filter/CharacterEncodingFilter.java
@@ -0,0 +1,27 @@
+package Filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = "/*")
+public class CharacterEncodingFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        System.out.println("enter the Filter");     // web服务器启动时，就开始初始化
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html;charset=UTF-8");
+//        System.out.println("change the charset");
+        chain.doFilter(request, response);  // 让请求和返回继续正常流程，如果没有这行代码，请求和返回会被拦截停止
+    }
+
+    @Override
+    public void destroy() {
+        System.out.println("leave the Filter");
+    }
+}
---
+++ Filter/src/main/java/Servlet/ShowServlet.java
@@ -0,0 +1,22 @@
+package Servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/show")
+public class ShowServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("UTF-8");
+        resp.getWriter().write("法撒看见发生发贺卡");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
